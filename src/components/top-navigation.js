e0f5563270053a10c7015946d1277f886b275611
Allan Joshua <calla@amazon.com>
2019-08-21 12:36:52 +0530

    Set avatar color

--- src/components/top-navigation.js
+++ src/components/top-navigation.js
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { Consumer } from '../context';
 import { Button, Menu, Avatar } from 'antd';
 import Credentials from '../utils/credentials';
+import toMaterialStyle from 'material-color-hash';
 
 const { SubMenu, Item } = Menu;
 
@@ -12,7 +13,7 @@ export default class TopNavigation extends React.Component {
         mode='horizontal'
       >
         <Item style={{ float: 'left' }}>Obiman</Item>
-        <SubMenu title={<Avatar src={profilePicture} children={email.substr(0,1).toUpperCase()} />} style={{ float: 'right' }}>
+        <SubMenu title={<Avatar style={toMaterialStyle(email)} src={profilePicture} children={email.substr(0,1).toUpperCase()} />} style={{ float: 'right' }}>
           <Item>My account</Item>
           <Item>My preferences</Item>
           <Item>
